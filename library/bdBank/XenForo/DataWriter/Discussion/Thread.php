767806ac17a5b10e9968f7eee548cf10e187b0d7
Dao Hoang Son <daohoangson@gmail.com>
2013-07-14 11:02:32 +0700

    fixed compatibility issue with XenForo 1.2.0 Beta 5

--- library/bdBank/XenForo/DataWriter/Discussion/Thread.php
+++ library/bdBank/XenForo/DataWriter/Discussion/Thread.php
@@ -1,14 +1,52 @@
 <?php
 
-class bdBank_XenForo_DataWriter_Discussion_Thread extends XFCP_bdBank_XenForo_DataWriter_Discussion_Thread {
-	protected function _discussionPostDelete(array $messages) {
-		$bank = bdBank_Model_Bank::getInstance(); 
+class bdBank_XenForo_DataWriter_Discussion_Thread_Base extends XFCP_bdBank_XenForo_DataWriter_Discussion_Thread
+{
+
+	protected function _bdBank_reversePostTransactions(array $postIds)
+	{
+		$bank = bdBank_Model_Bank::getInstance();
+
 		$comments = array();
-		foreach ($messages as $post_id => $post) {
-			$comments[] = $bank->comment('post',$post_id);
+		foreach ($postIds as $postId)
+		{
+			$comments[] = $bank->comment('post', $postId);
 		}
+
 		$bank->reverseSystemTransactionByComment($comments);
-		
-		return parent::_discussionPostDelete($messages);
+	}
+}
+
+if (XenForo_Application::$versionId < 1020000)
+{
+	// old versions
+	class bdBank_XenForo_DataWriter_Discussion_Thread extends bdBank_XenForo_DataWriter_Discussion_Thread_Base
+	{
+
+		protected function _discussionPostDelete(array $messages)
+		{
+			$this->_bdBank_reversePostTransactions(array_keys($messages));
+
+			return parent::_discussionPostDelete($messages);
+		}
+	}
+}
+else
+{
+	// v1.2+
+	class bdBank_XenForo_DataWriter_Discussion_Thread extends bdBank_XenForo_DataWriter_Discussion_Thread_Base
+	{
+
+		protected function _discussionPostDelete()
+		{
+			$messages = $this->_getDiscussionMessages(false); // do not panic, this method caches its results
+			if (!$messages)
+			{
+				return;
+			}
+			$this->_bdBank_reversePostTransactions(array_keys($messages));
+
+			return parent::_discussionPostDelete();
+		}
 	}
 }

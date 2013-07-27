55b3952d705583118768a09b742f495a8d20e450
Dao Hoang Son <daohoangson@gmail.com>
2013-07-27 22:04:50 +0700

    fixed bug with wrong javascript require path

--- library/bdBank/DevHelper/Config.php
+++ library/bdBank/DevHelper/Config.php
@@ -2,7 +2,7 @@
 class bdBank_DevHelper_Config extends DevHelper_Config_Base {
 	protected $_dataClasses = array();
 	protected $_dataPatches = array();
-	protected $_exportPath = '/Users/sondh/Dropbox/XenForo/bdBank';
+	protected $_exportPath = '/Users/sondh/XenForo/bdBank';
 	protected $_exportIncludes = array();
 	
 	/**

726ad027a0e215ff88c7a9ae93bc6e815ba23960
Dao Hoang Son <daohoangson@gmail.com>
2017-08-10 11:54:56 +0700

    v2.0.0 (+7) -- version bump

--- library/bdBank/ShippableHelper/TempFile.php
+++ library/bdBank/ShippableHelper/TempFile.php
@@ -1,10 +1,10 @@
 <?php
 
-// updated by DevHelper_Helper_ShippableHelper at 2016-08-08T20:19:03+00:00
+// updated by DevHelper_Helper_ShippableHelper at 2017-08-10T04:54:41+00:00
 
 /**
  * Class bdBank_ShippableHelper_TempFile
- * @version 6
+ * @version 12
  * @see DevHelper_Helper_ShippableHelper_TempFile
  */
 class bdBank_ShippableHelper_TempFile
@@ -13,6 +13,12 @@ class bdBank_ShippableHelper_TempFile
     protected static $_cached = array();
     protected static $_registeredShutdownFunction = false;
 
+    /**
+     * Downloads and put content into the specified temp file
+     *
+     * @param string $url
+     * @param string $tempFile
+     */
     public static function cache($url, $tempFile)
     {
         self::$_cached[$url] = $tempFile;
@@ -22,12 +28,25 @@ class bdBank_ShippableHelper_TempFile
         }
     }
 
-    public static function create($contents)
+    /**
+     * Creates a new temp file with the given contents and prefix.
+     *
+     * @param string|null $contents if null, no contents will be written to file
+     * @param string|null $prefix if null, the default prefix will be used
+     * @return string
+     */
+    public static function create($contents = null, $prefix = null)
     {
-        $tempFile = tempnam(XenForo_Helper_File::getTempDir(), self::_getPrefix());
+        if ($prefix === null) {
+            $prefix = self::_getPrefix();
+        }
+
+        $tempFile = tempnam(XenForo_Helper_File::getTempDir(), $prefix);
         self::cache(sprintf('%s::%s', __METHOD__, md5($tempFile)), $tempFile);
 
-        file_put_contents($tempFile, $contents);
+        if ($contents !== null) {
+            file_put_contents($tempFile, $contents);
+        }
 
         return $tempFile;
     }
@@ -39,7 +58,7 @@ class bdBank_ShippableHelper_TempFile
             'userAgent' => '',
             'timeOutInSeconds' => 0,
             'maxRedirect' => 3,
-            'maxDownloadSize' => 0,
+            'maxDownloadSize' => XenForo_Application::getOptions()->get('attachmentMaxFileSize') * 1024,
             'secured' => 0,
         );
 
@@ -47,19 +66,26 @@ class bdBank_ShippableHelper_TempFile
         $managedTempFile = false;
         if (strlen($tempFile) === 0) {
             $tempFile = tempnam(XenForo_Helper_File::getTempDir(), self::_getPrefix());
-            self::cache($url, $tempFile);
             $managedTempFile = true;
+        }
+        if (strlen($tempFile) === 0) {
+            return false;
         }
 
         if (isset(self::$_cached[$url])
             && filesize(self::$_cached[$url]) > 0
         ) {
             if ($managedTempFile) {
+                unlink($tempFile);
                 return self::$_cached[$url];
             } else {
                 copy(self::$_cached[$url], $tempFile);
                 return $tempFile;
             }
+        }
+
+        if ($managedTempFile) {
+            self::cache($url, $tempFile);
         }
 
         self::$_maxDownloadSize = $options['maxDownloadSize'];
@@ -91,23 +117,49 @@ class bdBank_ShippableHelper_TempFile
         }
 
         curl_exec($ch);
-
-        $downloaded = intval(curl_getinfo($ch, CURLINFO_HTTP_CODE)) == 200;
-
+        $curlInfo = curl_getinfo($ch);
         curl_close($ch);
-
         fclose($fh);
 
-        if (XenForo_Application::debugMode()) {
-            $fileSize = filesize($tempFile);
+        $downloaded = true;
+        if (!isset($curlInfo['http_code'])
+            || $curlInfo['http_code'] < 200
+            || $curlInfo['http_code'] >= 300
+        ) {
+            // no http response status / non success status, must be an error
+            $downloaded = false;
+        }
 
+        $fileSize = filesize($tempFile);
+        if ($downloaded && $fileSize === 0) {
+            clearstatcache();
+            $fileSize = filesize($tempFile);
+        }
+
+        if ($downloaded
+            && isset($curlInfo['size_download'])
+            && $fileSize !== intval($curlInfo['size_download'])
+        ) {
+            // file size reported by our system seems to be off, probably a write error
+            $downloaded = false;
+        }
+
+        if ($downloaded
+            && isset($curlInfo['download_content_length'])
+            && $curlInfo['download_content_length'] > 0
+            && $fileSize !== intval($curlInfo['download_content_length'])
+        ) {
+            // file size is different from Content-Length header, probably a cancelled download (or corrupted)
+            $downloaded = false;
+        }
+
+        if (XenForo_Application::debugMode()) {
             XenForo_Helper_File::log(__CLASS__, call_user_func_array('sprintf', array(
-                'download %s -> %s, %s, %d bytes%s',
+                'download %s -> %s, %s, %s',
                 $url,
                 $tempFile,
                 ($downloaded ? 'succeeded' : 'failed'),
-                $fileSize,
-                ((!$downloaded && $fileSize > 0) ? "\n\t" . file_get_contents($tempFile) : ''),
+                json_encode($curlInfo),
             )));
         }
 
